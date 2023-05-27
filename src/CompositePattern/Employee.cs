52a30b2fedbd4f3891f134f1b3c65786398fa880
engr-monirujjaman <engr.monirujjaman@yandex.com>
2023-05-27 22:09:50 +0600

    Added CompositePattern.csproj

---
+++ src/CompositePattern/Employee.cs
@@ -0,0 +1,31 @@
+﻿namespace CompositePattern;
+
+public class Employee
+{
+    public Employee(string name, string dept, int salary)
+    {
+        Name = name;
+        Dept = dept;
+        Salary = salary;
+        Subordinates = new List<Employee>();
+    }
+
+    public string Name { get; set; }
+
+    public string Dept { get; set; }
+
+    public int Salary { get; set; }
+
+    private List<Employee> Subordinates { get; set; }
+
+    public void Add(Employee employee) => Subordinates.Add(employee);
+
+    public void Remove(Employee employee) => Subordinates.Remove(employee);
+
+    public List<Employee> GetSubordinates() => Subordinates;
+
+    public override string ToString()
+    {
+        return $"Employee: [ Name: {Name}, Dept: {Dept}, Salary: {Salary} ]";
+    }
+}
---
+++ src/CompositePattern/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using CompositePattern;
+
+var ceo = new Employee("John","CEO", 30000);
+
+var headSales = new Employee("Robert","Head Sales", 20000);
+
+var headMarketing = new Employee("Michel","Head Marketing", 20000);
+
+var clerk1 = new Employee("Laura","Marketing", 10000);
+var clerk2 = new Employee("Bob","Marketing", 10000);
+
+var salesExecutive1 = new Employee("Richard","Sales", 10000);
+var salesExecutive2 = new Employee("Rob","Sales", 10000);
+
+ceo.Add(headSales);
+ceo.Add(headMarketing);
+
+headSales.Add(salesExecutive1);
+headSales.Add(salesExecutive2);
+
+headMarketing.Add(clerk1);
+headMarketing.Add(clerk2);
+
+Console.WriteLine(ceo);
+
+ceo.GetSubordinates().ForEach(x =>
+{
+    Console.WriteLine(x);
+    x.GetSubordinates().ForEach(Console.WriteLine);
+});
